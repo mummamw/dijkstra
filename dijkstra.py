b6945a64d8eec9418a9eaf6a87eeeb2b8a79f69b
Michael Mumma <Mummamw@miamioh.edu>
2015-11-13 16:36:24 -0500

    Added dijkstra.py for review

---
+++ dijkstra.py
@@ -0,0 +1,48 @@
+def dijkstra(graph,src,dest,visited=[],distances={},predecessors={}):
+
+    if src not in graph:
+        raise TypeError('the root of the shortest path tree cannot be found in the graph')
+    if dest not in graph:
+        raise TypeError('the target of the shortest path cannot be found in the graph')
+    # ending condition
+    if src == dest:
+        # We build the shortest path and display it
+        path=[]
+        pred=dest
+        while pred != None:
+            path.append(pred)
+            pred=predecessors.get(pred,None)
+        print('shortest path: '+str(path)+" cost="+str(distances[dest]))
+    else :
+        # if it is the initial  run, initializes the cost
+        if not visited:
+            distances[src]=0
+        # visit the neighbors
+        for neighbor in graph[src] :
+            if neighbor not in visited:
+                new_distance = distances[src] + graph[src][neighbor]
+                if new_distance < distances.get(neighbor,float('inf')):
+                    distances[neighbor] = new_distance
+                    predecessors[neighbor] = src
+        # mark as visited
+        visited.append(src)
+        # now that all neighbors have been visited: recurse
+        # select the non visited node with lowest distance 'x'
+        # run Dijskstra with src='x'
+        unvisited={}
+        for k in graph:
+            if k not in visited:
+                unvisited[k] = distances.get(k,float('inf'))
+        x=min(unvisited, key=unvisited.get)
+        dijkstra(graph,x,dest,visited,distances,predecessors)
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    #unittest.main()
+    graph = {'s': {'a': 100, 'b': 1444},
+            'a': {'s': 333, 'b': 444, 'c':118},
+            'b': {'s': 422, 'a': 233, 'd': 222},
+            'c': {'a': 22, 'd': 722, 't': 444},
+            'd': {'b': 11, 'c': 111, 't': 555},
+            't': {'c': 313, 'd': 115}}
+    dijkstra(graph,'s','t')
